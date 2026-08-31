85d4be7f42ec75906c432ceb9213f36099451cdc
Joel Hoskins <323173345+joelhoskins36@users.noreply.github.com>
2026-08-31 12:06:21 -0400

    Initial Java bootcamp setup

---
+++ examples/module-01-exercises/WoraProbe.java
@@ -0,0 +1,8 @@
+public class WoraProbe {
+    public static void main(String[] args) {
+        var os =System.getProperty("os.name");
+        System.out.println (os);
+        System.out.print ("Bytecode runs on: " + os);
+
+    }
+}
---
+++ notes/md-docs/module-01-notes/wora-note.md
@@ -0,0 +1,4 @@
+When compiling either hello or Woraprobe,
+Since it is already compiled it iis anble
+Be ran anywhere. Also, It can fill in des
+-crepencies depending on the command.
