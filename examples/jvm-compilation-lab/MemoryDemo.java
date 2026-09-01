d2fbacf1b119edc3d9f360d0fa65269d37b398ce
Joel Hoskins <323173345+joelhoskins36@users.noreply.github.com>
2026-09-01 11:28:31 -0400

    finished mem demo

--- examples/jvm-compilation-lab/MemoryDemo.java
+++ examples/jvm-compilation-lab/MemoryDemo.java
@@ -6,7 +6,8 @@ public class MemoryDemo {
         List<Employee> employees = new ArrayList<>();
 
         // TODO: loop i from 1 to 100000; add new Employee(i, "Employee-" + i)
-        // TODO: print "Created " + employees.size() + " employees"
-        throw new UnsupportedOperationException("TODO");
+        for(int i=1;i<=100000;i++)
+            employees.add(new Employee(i, "Employee-" + i));
+        System.out.println("Created "+ employees.size()+ " employees");
     }
 }
